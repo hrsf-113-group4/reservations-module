44d6d5ce21ffd395e26ecb42f25213dfacfdeff2
Peter R <pryzanych@gmail.com>
2019-04-08 12:28:48 -0700

    Create CSS styling for all components using Styled Components.

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,42 @@
 import React from 'react';
 import moment from 'moment';
+import styled from 'styled-components';
 import PartySize from './PartySize.jsx'
 import FindReservation from './FindReservation.jsx';
 import ReservationOption from './ReservationOption.jsx';
 import ReservationTime from './ReservationTime.jsx';
 import DatePicker from './DatePicker.jsx';
-import Calendar from './Calendar.jsx';
+
+const Wrapper = styled.div`
+  display: block;
+  background-color: #ffffff;
+  box-shadow: 0 2px 8px rgba(153,153,153,.4);
+  font-family: BrandonText,-apple-system,BlinkMacSystemFont,Segoe UI,Roboto,Helvetica,Arial,sans-serif,Apple Color Emoji,Segoe UI Emoji,Segoe UI Symbol;
+`;
+
+const Title = styled.h3`
+  justify-content: center;
+  height: 100%;
+  display: block;
+  font-size: 1.17em;
+  margin-block-start: 1em;
+  margin-block-end: 1em;
+  margin-inline-start: 0px;
+  margin-inline-end: 0px;
+  font-weight: bold;
+  flex-direction: column;
+  border-bottom: 1px solid #d8d9db;
+  text-align: center;
+`;
+
+const ButtonSpace = styled.div`
+  justify-content: center;
+  text-align: center;
+  margin: 1rem auto 0;
+  overflow: hidden;
+  width: 100%;
+
+`;
 
 class App extends React.Component {
   constructor(props) {
@@ -35,7 +66,6 @@ class App extends React.Component {
 
   renderButtons() {
     const {buttons} = this.state;
-
     if (buttons === 'reservation') {
       return <FindReservation />
     } else if (buttons === 'times') {
@@ -45,16 +75,16 @@ class App extends React.Component {
 
   render() {
     return (
-      <div>
-        <h2 className='title'>Make a reservation</h2>
+      <Wrapper>
+        <Title>Make a reservation</Title>
         <div className='party'><PartySize /></div>
         <div className='reservation-date'><DatePicker firstDay={this.firstDayOfTheMonth} date={this.state.date} handleCalendar={this.handleCalendar} displayCalendar={this.state.displayCalendar}/></div>
         <div className='reservation-time'><ReservationTime time={this.state.time}/></div>
-        <div className='find-a-table'>
+        <ButtonSpace>
           {this.renderButtons()}
-        </div>
+        </ButtonSpace>
         <div className='reservation-info'></div>
-      </div>
+      </Wrapper>
     )
   }
 }
--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,5 +1,89 @@
 import React from 'react';
 import moment from 'moment';
+import styled from 'styled-components';
+
+const CalendarBase = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  position: relative;
+  user-select: none;
+  flex-direction: row;
+  padding: 1rem 0;
+  margin: 0 auto;
+  border: 1px solid #d8d9db;
+  background-color: #f1f2f4;
+  width: 100%;
+`;
+
+const CalendarTable = styled.div`
+  display: table;
+  table-layout: fixed;
+  border-collapse: collapse;
+  border-spacing: 0;
+  user-select: none;
+  margin: 0 1rem;
+  width: 256px;
+`;
+
+const MonthButton = styled.span`
+  display: flex;
+  position: absolute;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 6px 8px;
+  background-image: url(data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 5….5 0 0 0 .71 0L4.39 5.1l.71-.71a.5.5 0 0 0-.01-.71z'/%3E%3C/g%3E%3C/svg%3E);
+  cursor: pointer;
+  width: 32px;
+  height: 32px;
+  border: 1px solid #d8d9db;
+  border-radius: 50%;
+  box-sizing: border-box;
+  right: 1rem;
+`;
+
+const LastMonthButton = styled.span`
+  display: flex;
+  position: absolute;
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 6px 8px;
+  background-image: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 5.24 8.07'%3E%3Cg%3E%3Cpath style='fill:%23E1E1E1' d='M5.09 3.68L4.39 3 1.56.15a.5.5 0 0 0-.71 0l-.7.7a.5.5 0 0 0 0 .71L2.62 4 .15 6.51a.5.5 0 0 0 0 .71l.71.71a.5.5 0 0 0 .71 0L4.39 5.1l.71-.71a.5.5 0 0 0-.01-.71z'/%3E%3C/g%3E%3C/svg%3E");
+  cursor: default;
+  width: 32px;
+  height: 32px;
+  border: 1px solid #d8d9db;
+  border-radius: 50%;
+  box-sizing: border-box;
+  pointer-events: none;
+  left: 1rem;
+`;
+
+const Heading = styled.div`
+  display: table-caption;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  font-weight: 700;
+`;
+
+const TableCell = styled.td`
+  display: table-cell;
+  text-align: center;
+  cursor: pointer;
+  vertical-align: middle;
+  border: 1px solid #d8d9db;
+  border-collapse: collapse;
+  border-spacing: 0;
+  background-color: #fff;
+  box-sizing: border-box;
+  position: relative;
+  font-weight: 500;
+  background-clip: padding-box;
+  line-height: 33px;
+`;
 
 const Calendar = ({firstDay, date}) => { 
   let shortWeekday = moment.weekdaysShort();
@@ -11,12 +95,12 @@ const Calendar = ({firstDay, date}) => {
       </th>
     );
   });
-  
+
   let monthBuilder = () => {
     let previousMonth = [];
     for (var i = 0; i < firstDay(); i++) {
       previousMonth.unshift(
-        <td className='weekday last-month'>{moment(date).startOf('month').subtract(i + 1, 'days').format('D')}</td>
+        <TableCell className='weekday last-month'>{moment(date).startOf('month').subtract(i + 1, 'days').format('D')}</TableCell>
       );
     };
 
@@ -24,18 +108,18 @@ const Calendar = ({firstDay, date}) => {
     for (var j = 1; j <= moment(date).daysInMonth(); j++) {
       let chosenDay = j == currentDay ? 'chosen' : '';
       monthDays.push(
-        <td className={`weekday month ${chosenDay}`}>
+        <TableCell className={`${chosenDay}`}>
           {j}
-        </td>
+        </TableCell>
       );
     };
 
     let nextMonth = [];
     for (var k = 1; k < moment(date).add(1, 'months').startOf('month').endOf('week').format('d') - 1; k++) {
       nextMonth.push(
-        <td className='weekday next-month'>
+        <TableCell className='weekday next-month'>
           {k}
-        </td>
+        </TableCell>
       )
     }
     
@@ -64,15 +148,17 @@ const Calendar = ({firstDay, date}) => {
   };
 
   return (
-    <div className='calendar'>
-      <span id='last-month-button' role='button'>Last Month</span>
-      <div id='current-month'>{moment(date).format('MMMM YYYY')}</div>
-      <span id='next-month-button' role='button'>Next Month</span>
-      <table className='calendar'>
-        <thead><tr>{weekdayHeader}</tr></thead>
-        <tbody>{monthBuilder()}</tbody>
-      </table>
-    </div>
+    <CalendarBase>
+      <CalendarTable>
+        <LastMonthButton id='last-month-button' role='button'></LastMonthButton>
+        <Heading>{moment(date).format('MMMM YYYY')}</Heading>
+        <MonthButton id='next-month-button' role='button'></MonthButton>
+        <table className='calendar'>
+          <thead><tr>{weekdayHeader}</tr></thead>
+          <tbody>{monthBuilder()}</tbody>
+        </table>
+      </CalendarTable>
+    </CalendarBase>
   )
 }
 
